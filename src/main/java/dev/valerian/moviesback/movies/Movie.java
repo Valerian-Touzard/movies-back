10f1bf9a4a6611efd53a5ef789a389f4e0070855
Valerian <vtouzard244@gmail.com>
2023-11-06 16:10:54 +0100

    feat: Complétion du modèle Movie

--- src/main/java/dev/valerian/moviesback/movies/Movie.java
+++ src/main/java/dev/valerian/moviesback/movies/Movie.java
@@ -1,11 +1,13 @@
 package dev.valerian.moviesback.movies;
 
+import dev.valerian.moviesback.review.Review;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -23,4 +25,6 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
 }
